7edcdc1b433e6eaa9c609731453cd104ae09110e
Chandan <chandan.j080@gmail.com>
2026-01-28 08:37:09 -0500

    updated sign up route and component

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, output, signal } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import {
   IonContent,
   IonCard,
@@ -12,9 +12,15 @@ import {
   IonSpinner,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { cubeOutline } from 'ionicons/icons';
+import { arrowBackOutline, cubeOutline, personAddOutline } from 'ionicons/icons';
 
 export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface SignupCredentials {
+  fullName: string;
   email: string;
   password: string;
 }
@@ -39,20 +45,51 @@ export interface LoginCredentials {
 })
 export class LoginComponent {
   loginSubmit = output<LoginCredentials>();
+  signupSubmit = output<SignupCredentials>();
   googleSignIn = output<void>();
 
   loginForm: FormGroup;
+  signupForm: FormGroup;
   isLoading = signal(false);
   isGoogleLoading = signal(false);
+  isSignupLoading = signal(false);
+  showSignup = signal(false);
 
   constructor(private fb: FormBuilder) {
-    addIcons({ cubeOutline });
+    addIcons({ cubeOutline, personAddOutline, arrowBackOutline });
 
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+
+    this.signupForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), this.passwordStrengthValidator()]],
+      confirmPassword: ['', [Validators.required]],
+    }, { validators: this.passwordMatchValidator });
   }
+
+  private passwordStrengthValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) return null;
+
+      const hasUpperCase = /[A-Z]/.test(value);
+      const hasLowerCase = /[a-z]/.test(value);
+      const hasNumeric = /[0-9]/.test(value);
+
+      const valid = hasUpperCase && hasLowerCase && hasNumeric;
+      return valid ? null : { passwordStrength: true };
+    };
+  }
+
+  private passwordMatchValidator: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  };
 
   onSubmit(): void {
     if (this.loginForm.valid) {
@@ -68,11 +105,43 @@ export class LoginComponent {
     this.googleSignIn.emit();
   }
 
+  onSignupSubmit(): void {
+    if (this.signupForm.valid) {
+      this.isSignupLoading.set(true);
+      const { confirmPassword, ...credentials } = this.signupForm.value;
+      this.signupSubmit.emit(credentials as SignupCredentials);
+    } else {
+      this.signupForm.markAllAsTouched();
+    }
+  }
+
+  toggleSignup(): void {
+    this.showSignup.update(v => !v);
+    this.signupForm.reset();
+    this.loginForm.reset();
+  }
+
   get emailControl() {
     return this.loginForm.get('email');
   }
 
   get passwordControl() {
     return this.loginForm.get('password');
+  }
+
+  get signupFullNameControl() {
+    return this.signupForm.get('fullName');
+  }
+
+  get signupEmailControl() {
+    return this.signupForm.get('email');
+  }
+
+  get signupPasswordControl() {
+    return this.signupForm.get('password');
+  }
+
+  get signupConfirmPasswordControl() {
+    return this.signupForm.get('confirmPassword');
   }
 }
